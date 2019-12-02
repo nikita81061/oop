319aa469bfb448d46d4789838229c71cd055e378
nikita81061 <nikitazaytsev20@gmail.com>
2019-12-02 19:18:47 +0200

    oop

---
+++ ConsoleApp2/Fraction.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionClass
+{
+    public class Fraction
+    {
+        protected long Numerator;
+        protected long Denominator;
+        public Fraction(long numer, long denom)
+        {
+            Numerator = numer;
+            Denominator = denom;
+            Fix();
+            Simplify();
+        }
+        public Fraction(long numer)
+        {
+            Numerator = numer;
+            Denominator = 1;
+        }
+        public Fraction(double l)
+        {
+            Denominator = 1;
+            while (l > (long)l)
+            {
+                l *= 10;
+                Denominator *= 10;
+            }
+            Numerator = (long)l;
+            Simplify();
+        }
+        public Fraction(double num, double den)
+        {
+            Denominator = 1;
+            while ((num > (long)num) || (den > (long)den))
+            {
+                num *= 10;
+                den *= 10;
+            }
+            Numerator = (long)num;
+            Denominator = (long)den;
+            Simplify();
+        }
+        public static Fraction operator -(Fraction a)
+        {
+            return new Fraction(-a.Numerator, a.Denominator);
+        }
+        public static Fraction operator +(Fraction a)
+        {
+            return new Fraction(a.Numerator, a.Denominator);
+        }
+        public static Fraction operator !(Fraction a)
+        {
+            Fraction res = new Fraction(a.Denominator, a.Numerator);
+            res.Fix();
+            return res;
+        }
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Numerator * b.Denominator + a.Denominator * b.Numerator, a.Denominator * b.Denominator);
+        }
+        public static Fraction operator +(Fraction a, long b)
+        {
+            return a + new Fraction(b);
+        }
+        public static Fraction operator +(long a, Fraction b)
+        {
+            return new Fraction(a) + b;
+        }
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Numerator * b.Denominator - a.Denominator * b.Numerator, a.Denominator * b.Denominator);
+        }
+        public static Fraction operator -(Fraction a, long b)
+        {
+            return a - new Fraction(b);
+        }
+        public static Fraction operator -(long a, Fraction b)
+        {
+            return new Fraction(a) - b;
+        }
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+        }
+        public static Fraction operator *(Fraction a, long b)
+        {
+            return a * new Fraction(b);
+        }
+        public static Fraction operator *(long a, Fraction b)
+        {
+            return new Fraction(a) * b;
+        }
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return a * !b;
+        }
+        public static Fraction operator /(Fraction a, long b)
+        {
+            return a * !(new Fraction(b));
+        }
+        public static Fraction operator /(long a, Fraction b)
+        {
+            return new Fraction(a) * !b;
+        }
+
+        public void Simplify()
+        {
+            long num = Numerator;
+            long den = Denominator;
+            if (Numerator<0) {
+                num *= -1;
+            }
+            while ((num != 0) && (den != 0))
+            {
+                if (num > den)
+                     num %= den;
+                else
+                    den %= num;
+            }
+            long nod = Math.Max(num, den);
+            Numerator /= nod;
+            Denominator /= nod;
+        }
+        public void Fix()
+        {
+            if ((Numerator <= 0 && Denominator <= 0) || (Denominator <= 0 && Numerator >= 0))
+            {
+                Numerator = -Numerator;
+                Denominator = -Denominator;
+            }
+        }
+        public override string ToString()
+        {
+            string s = "Inf";
+            if (Denominator == 0)
+                return s;
+            else
+                return $"{Numerator}/{Denominator}";
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FractionClass;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction a = new Fraction(1, 2);
+            Fraction b = new Fraction(5, 7);
+            Fraction c = new Fraction(17);
+            Fraction d = new Fraction(1.7);
+            Fraction res = a + b;
+            Console.WriteLine(a-c);
+            Console.WriteLine(b+d);
+            Console.WriteLine(b/a);
+            Console.WriteLine(a*d);
+            Console.WriteLine(-a);
+            Console.WriteLine(a+c/d);
+
+        }
+    } 
+}
